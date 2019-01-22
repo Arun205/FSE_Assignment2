1a58642564c4fa30c68231c5d66d6a29be0f0b50
Arun205 <36626268+Arun205@users.noreply.github.com>
2019-01-22 13:59:02 +0530

    redesigned using material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatTooltipModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDividerModule,
+  MatCardModule } from '@angular/material'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +12,7 @@ import { PlayerComponent } from './videoplayer/player/player.component';
 import { ControlsComponent } from './videoplayer/controls/controls.component';
 import { PlaylistComponent } from './videoplayer/playlist/playlist.component';
 import { AddvideoComponent } from './addvideo/addvideo.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,22 @@ import { AddvideoComponent } from './addvideo/addvideo.component';
     PlayerComponent,
     ControlsComponent,
     PlaylistComponent,
-    AddvideoComponent
+    AddvideoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
