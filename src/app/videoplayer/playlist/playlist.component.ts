5b4e9f646e637c0aa4d17033109ed192991b6c12
Arun205 <36626268+Arun205@users.noreply.github.com>
2019-01-24 16:22:45 +0530

    controls and playlist

--- src/app/videoplayer/playlist/playlist.component.ts
+++ src/app/videoplayer/playlist/playlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
+import { VideoData } from './../../models/videodata.model';
 
 @Component({
   selector: 'app-playlist',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaylistComponent implements OnInit {
 
-  constructor() { }
+  public allvideos_url = 'http://localhost:3000/dataservice';
+  public videos: VideoData[];
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.http.get(this.allvideos_url)
+    .toPromise()
+    .then((res) => res.json())
+    .then((data) => {
+      const allvideos = data;
+      this.videos = [];
+      for (let i in allvideos) {
+        if (allvideos[i].status == 'Approved') {
+          this.videos.push(allvideos[i]);
+        }
+      }
+      console.log(this.videos);
+    });
   }
-
 }
